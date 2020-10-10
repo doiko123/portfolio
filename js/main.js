e1f64f1e22fd22718d667d24810501eaa7f4064d
doiko123 <ayanottyz@gmail.com>
2020-10-10 11:03:36 +0900

    ページトップに戻るボタンのjs追記＋それに伴うindex.htmlとcss微修正

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 'use strict';
 
+// anime.js利用
 
+/*スクロールでトップに戻るボタンを表示*/
+const pxChange = 1;
 
-
-document.getElementById('goToPageTop').addEventListener("click", function () {
-
-  let height = window.pageYOffset;
-  let scrollTimer = 15;
-  let scrollBase = height / scrollTimer;
-
-  const scrollPageTop = () => {
-
-    scrollTo(0, height - scrollBase);
-
-  scrollPageTop();
+window.addEventListener('scroll', function (e) {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > pxChange) {
+    document.getElementById("goToPageTopLink").classList.add("fadeIn");
+  } else {
+    document.getElementById("goToPageTopLink").classList.remove("fadeIn");
   }
-};
+});
 
+/*トップに戻るボタンのスムーズスクロール*/
+document.getElementById("goToPageTopLink").addEventListener('click', function (e) {
+  anime.remove("html, body");
+  anime({
+    targets: "html, body",
+    scrollTop: 0,
+    dulation: 600,
+    easing: 'easeOutCubic',
+  });
+  return false;
+});
