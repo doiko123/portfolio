0a24b44678637beefbb35e79c4cb4ec893af82ce
doiko123 <ayanottyz@gmail.com>
2020-10-10 18:50:51 +0900

    main.jsにお問い合わせフォームのメールアドレスバリデーションを追記

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // anime.js利用
 
-/*スクロールでトップに戻るボタンを表示*/
+/* スクロールでトップに戻るボタンを表示 */
 const pxChange = 1;
 
 window.addEventListener('scroll', function (e) {
@@ -14,7 +14,7 @@ window.addEventListener('scroll', function (e) {
   }
 });
 
-/*トップに戻るボタンのスムーズスクロール*/
+/* トップに戻るボタンのスムーズスクロール */
 document.getElementById("goToPageTopLink").addEventListener('click', function (e) {
   anime.remove("html, body");
   anime({
@@ -25,3 +25,20 @@ document.getElementById("goToPageTopLink").addEventListener('click', function (e
   });
   return false;
 });
+
+/* contact.htmlフォームのメールアドレス バリデーションチェック */
+function inputValueCheck() {
+  const inputEmail = document.getElementById("form-mail").value;
+  checkInputEmail(inputEmail);
+}
+
+//指定したメールアドレスの形式と異なる場合は弾いてアラートmsgを表示
+function checkInputEmail(inputEmail) {
+  const inputEmailPattern = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/ //入力ルール（正規表現）
+
+  if (inputEmailPattern.test(inputEmail)) {
+    return submitted = true;
+  } else {
+    alert("メールアドレスの形式に誤りがあります。ご確認をお願いいたします。")
+  }
+}
